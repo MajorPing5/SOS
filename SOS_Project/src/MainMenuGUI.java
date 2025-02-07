5f181a7ea56cb1735ab37327fdb9fb7996aa7bf0
Robert-Abel Faustino Porras <majorping5@gmail.com>
2025-02-07 00:12:02 -0600

    Minor Config Updates
    Window now boots online when called. Unit Tests are next on the list, and will require some elbow grease for the code to be in a state of more operational vs placeholder.

---
+++ SOS_Project/src/MainMenuGUI.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class MainMenuGUI extends Application{
+	public void start(Stage stage) {
+		Circle circ = new Circle(40, 40, 30);
+		Group root = new Group(circ);
+		Scene scene = new Scene(root, 400, 300);
+		
+		stage.setTitle("SOS Project");
+		stage.setScene(scene);;
+		stage.show();
+	}
+	
+}
